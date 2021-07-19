39780101c5aaf1855142aab924b6a4008a246ac4
Ravil <isxakovravil82@gmail.com>
2021-07-19 12:03:40 +0600

    ава

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNum, secondNum;
+            double result;
+
+            Console.WriteLine("Введите первое число");
+            firstNum = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число");
+            secondNum = double.Parse(Console.ReadLine());
+
+            result = (firstNum + secondNum) / 2;
+
+            Console.WriteLine("Среднее арифметическое число: " + result);
+        }
+    }
+}
